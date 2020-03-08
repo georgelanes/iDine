639c34713d8f2e0c016e239e00ab1c010ed94d9e
George <georgelanes@gmail.com>
2020-03-07 23:28:39 -0300

    Adding supports files (models, helpers, imagens and fake json data).

---
+++ iDine/Models/Order.swift
@@ -0,0 +1,31 @@
+//
+//  Order.swift
+//  iDine
+//
+//  Created by George Nascimento on 07/03/20.
+//  Copyright © 2020 GLOBIT. All rights reserved.
+//
+
+import SwiftUI
+
+class Order {
+    var items = [MenuItem]()
+
+    var total: Int {
+        if items.count > 0 {
+            return items.reduce(0) { $0 + $1.price }
+        } else {
+            return 0
+        }
+    }
+
+    func add(item: MenuItem) {
+        items.append(item)
+    }
+
+    func remove(item: MenuItem) {
+        if let index = items.firstIndex(of: item) {
+            items.remove(at: index)
+        }
+    }
+}
