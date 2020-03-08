ced4f3a8c7fe1b5983a27bb31e5fc4c6f88c04ae
George <georgelanes@gmail.com>
2020-03-08 02:39:43 -0300

    Added confirm order alert.

--- iDine/Views/CheckoutView.swift
+++ iDine/Views/CheckoutView.swift
@@ -18,6 +18,7 @@ struct CheckoutView: View {
     @State private var addLoyaltyDetails = false
     @State private var loyaltyNumber = ""
     @State private var tipAmount = 1
+    @State private var showingPaymentAlert = false
     
     var totalPrice: Double {
         let total = Double(order.total)
@@ -53,11 +54,16 @@ struct CheckoutView: View {
             
             Section(header:Text("TOTAL: $\(totalPrice, specifier: "%.2f")")) {
                 Button("Confirm order") {
-                    // ss
+                    self.showingPaymentAlert.toggle()
                 }
             }
         }
         .navigationBarTitle(Text("Payment"), displayMode: .inline)
+        .alert(isPresented: $showingPaymentAlert) {
+            Alert(
+                title: Text("Order Confirmed"),
+                message: Text("Your total was $\(totalPrice, specifier: "%.2f") - thank you!"), dismissButton: .default(Text("OK")))
+        }
     }
 }
 
