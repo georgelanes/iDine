3f44b4c46d53631f78450f6e4dda1615c9e3d6f0
George <georgelanes@gmail.com>
2020-03-07 23:01:42 -0300

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by George Nascimento on 07/03/20.
+//  Copyright © 2020 GLOBIT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
